e5d8cb2cc12f93f4d4345b26371b65a1201b34e6
kittu246 <119164660+kittu246@users.noreply.github.com>
2023-09-19 15:55:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let child = document.getElementById("level");
+
+console.log(child.parentNode);
+let i=0;
+
+while(child.parentNode){
+  child = child.parentNode;
+  i++;
+}
+
+window.alert(`The level of the element is: ${i}`);
